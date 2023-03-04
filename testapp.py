746842a233fc3efa3c1058ea68907398d0c37225
freinric <freinric@gmail.com>
2023-03-04 11:09:53 -0800

    cleaning doc

--- testapp.py
+++ testapp.py
@@ -6,19 +6,36 @@ import dash                                     # pip install dash
 from dash import dcc, html, Input, Output, html
 import plotly.express as px
 
-# provinces?
+# POSSIBLE LINKS FOR TOPJSON
 # https://gist.github.com/Saw-mon-and-Natalie/a11f058fc0dcce9343b02498a46b3d44?short_path=2b4dce3
 # https://gist.github.com/jdylanmc/a3fd5ca8c960eaa4b4354445b4480dad?short_path=9a3cad4
 
-df = pd.read_csv("data_extra.csv")  # https://drive.google.com/file/d/1m63TNoZdDUtH5XhK-mc4kDFzO9j97eWW/view?usp=sharing
+### TABLE OF CONTENTS ###
+# DEFINING
+# PLOT FUNCTIONS
+# APP LAYOUT
+# CALLBACK
+
+#------------------------------------------------------------------------------
+# DEFINING
+df = pd.read_csv("data_extra.csv")  
 provs = [x for x in df['province'].unique()]
 
+colors = {
+    'background': 'dark',
+    'background_dropdown': '#DDDDDD',
+    'H1':'#00BBFF',
+    'H3': '#7FDBFF'
+}
+style_dropdown = {'width': '100%', 'font-family': 'arial', "font-size": "1.1em", "background-color": colors['background_dropdown'], 'font-weight': 'bold'}
+
+#------------------------------------------------------------------------------
 ### PLOT 1 FUNCTION ###
-def plot_altair1(dff):
-    barchart = alt.Chart(dff[-pd.isnull(dff['milk_1L'])]).mark_bar().encode(
-    alt.X('milk_1L', title='Cost', axis=alt.Axis(orient='top',format='$')),
+def plot_altair1(dff, drop1_chosen):
+    barchart = alt.Chart(dff[-pd.isnull(dff[drop1_chosen])]).mark_bar().encode(
+    alt.X(drop1_chosen, title='Cost of '+drop1_chosen, axis=alt.Axis(orient='top',format='$')),
     alt.Y('city', sort='x', title=""),
-    tooltip=['milk_1L','province'])
+    tooltip=[drop1_chosen,'province'])
     return barchart.to_html()
 
 ### PLOT 2 FUNCTION ###
@@ -36,8 +53,9 @@ def plot_altair2(drop_a, drop_b, value):
     ).configure_axis(labelFontSize = 16, titleFontSize=20)
     return chart.to_html()"""
 
-app = dash.Dash(__name__)
 #------------------------------------------------------------------------------
+app = dash.Dash(__name__)
+
 app.layout = html.Div([
 
         html.Div([
@@ -71,13 +89,20 @@ app.layout = html.Div([
                 #persistence_type='',                   # stores user's changes to dropdown in memory ( I go over this in detail in Dropdown video: https://youtu.be/UYH_dNSX1DM )
             ),
             ### SLIDER ###
-            dcc.RangeSlider(id="population", min=0, max=4000000, value=[0,4000000])
+            dcc.RangeSlider(id="population", min=0, max=4000000, value=[0,4000000]),
+            ### DROPDOWN 1 ###
+            dcc.Dropdown(
+                    id='drop1',
+                    placeholder="Variables",
+                    value='meal_cheap',  # REQUIRED to show the plot on the first page load
+                    options=[{'label': col, 'value': col} for col in df.columns], 
+                    style = style_dropdown)
         ]),
-### GRAPH ### 
+        ### GRAPH ### 
         html.Div([
             html.Iframe(
                 id='plot1',
-                srcDoc=plot_altair1(df),
+                srcDoc=plot_altair1(df, drop1_chosen='meal_cheap'),
                 style={'border-width':'0', 'width':'100%','height':'400px'}
             )
     ]),
@@ -90,15 +115,18 @@ app.layout = html.Div([
     Output(component_id='plot1', component_property='srcDoc'),
     Output('prov_checklist', 'value'),
     [Input(component_id='prov_checklist', component_property='value'),
-     Input('population', 'value')]
+     Input('population', 'value'),
+     Input('drop1', 'value'),]
 )
-def update_df(options_chosen, population_chosen):
+def update_df(options_chosen, population_chosen, drop1_chosen):
+
+    # filter by population
     popmin = population_chosen[0]
     popmax = population_chosen[1]
     dff = df[df['population'].between(popmin, popmax)]
-
+    
+    # filtering by provinces chosen + updating the checkboxes with 'select all'
     if "all" in options_chosen and len(options_chosen) == 13: # want 'all' only highlighted when len = 14
-        #only time everything is highlighted is when 'all', so if not, 'all' not highlighted
         options_chosen.remove('all') # remove 'all' from list, unhighlight
         dff = dff[dff['province'].isin(options_chosen)] # new df of filtered list
     elif "all" in options_chosen: # if 'all' is selected
@@ -109,8 +137,8 @@ def update_df(options_chosen, population_chosen):
         dff = dff
     else: # in all other cases where not 'all'
         dff = dff[dff['province'].isin(options_chosen)]
-    
-    return plot_altair1(dff), options_chosen
+
+    return plot_altair1(dff, drop1_chosen), options_chosen
 
 
 #------------------------------------------------------------------------------
